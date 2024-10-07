6b4a367add691eb33642f523971e665137365c2f
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2024-10-07 06:18:10 +0000

    Fixed

--- function/scraper/goodbye.js
+++ function/scraper/goodbye.js
@@ -5,9 +5,9 @@ const path = require('path');
 exports.config = {
     name: 'goodbye',
     author: 'Lance Cochangco',
-    description: 'Generate a goodbye canvas image',
+    description: 'Generate a goodbye image',
     category: 'canvas',
-    link: ['/canvas/goodbye?pp=https://i.imgur.com/xwCoQ5H.jpeg&nama=Lance&bg=https://i.ibb.co/4YBNyvP/images-76.jpg&member=25']
+    link: ['/goodbye?pp=https://i.imgur.com/xwCoQ5H.jpeg&nama=Lance&bg=https://i.ibb.co/4YBNyvP/images-76.jpg&member=25']
 };
 
 exports.initialize = async function ({ req, res }) {
