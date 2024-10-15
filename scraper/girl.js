f98f00fe3405cfbf3c28506f56acc5613f956454
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2024-10-15 04:34:07 +0000

    Hey

---
+++ scraper/girl.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+
+exports.config = {
+    name: 'girl',
+    author: 'Đức tài cuti vcl',
+    description: 'Random girl photo',
+    category: 'random',
+    link: ['/girl']
+};
+
+exports.initialize = async function ({ req, res }) {
+    try {
+        const response = await axios.get('https://raw.githubusercontent.com/ajirodesu/Rest-Api-Assets/refs/heads/main/txt/girl.txt');
+        const data = response.data.split(/\r?\n/);
+
+        const link = data[Math.floor(Math.random() * data.length)].trim();
+        const link1 = data[Math.floor(Math.random() * data.length)].trim();
+
+        res.json({
+            "Authors": this.config.author,
+            "url": `${link}`,
+            "data": `${link1}`
+        });
+    } catch (error) {
+        console.error("Error fetching cosplay data:", error);
+        res.status(500).json({ error: "Failed to fetch cosplay data" });
+    }
+};
