54c57432db359a1813b4f21fc8c52dbb2f2fd198
Lance Cochangco <lancecochangco@yahoo.com>
2025-01-02 03:49:20 +0000

    Ey

---
+++ includes/assets/js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                blue: {"500":"#2563eb"},
+                gray: {
+                    "800": "#1e1e1e",
+                    "900": "#0c0c0c"
+                }
+            },
+            fontFamily: {
+                'sans': ['Playfair Display', 'serif'],
+            },
+        },
+    },
+}
+
